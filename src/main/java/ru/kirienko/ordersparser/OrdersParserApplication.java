e7a67c0323995aa971006f8a9fb9876b2a3539dd
Andrey Kirienko <kirienko.av@outlook.com>
2019-05-31 11:14:47 +1000

    Initialize

---
+++ src/main/java/ru/kirienko/ordersparser/OrdersParserApplication.java
@@ -0,0 +1,13 @@
+package ru.kirienko.ordersparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersParserApplication.class, args);
+	}
+
+}
