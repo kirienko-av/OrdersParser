a917123156e48b902b073f0312647d3934320682
Andrey Kirienko <kirienko.av@outlook.com>
2019-06-05 11:54:06 +1000

    Add comand line support

--- src/main/java/ru/kirienko/ordersparser/OrdersParserApplication.java
+++ src/main/java/ru/kirienko/ordersparser/OrdersParserApplication.java
@@ -3,7 +3,6 @@ package ru.kirienko.ordersparser;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import ru.kirienko.ordersparser.integration.IntegrationConfiguration;
 import ru.kirienko.ordersparser.integration.OrderParserGateway;
 
 import java.util.Arrays;
@@ -14,8 +13,6 @@ public class OrdersParserApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new AnnotationConfigApplicationContext(OrdersParserApplication.class);
 		OrderParserGateway orderGateway = ctx.getBean(OrderParserGateway.class);
-
-		orderGateway.parse(Arrays.asList("D:\\install.log"));
+		orderGateway.parse(Arrays.asList(args));
 	}
-
 }
